799a1b5d5353d5f62a4e8eccc57ac4e1d488d707
pixelpc <pixelpc@localhost.localdomain>
2018-10-20 16:07:49 +0200

    Second ajout : -Premiers fichiers CPP

---
+++ includers/STORYONE.h
@@ -0,0 +1,11 @@
+#ifndef STORYONE_H
+#define STORYONE_H
+
+class STORYONE {
+private:
+
+public:
+    
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "includers/STORYONE.h"
+
+int main(int argc, char const *argv[]) {
+//Début du bordel !
+
+return 0;
+}
